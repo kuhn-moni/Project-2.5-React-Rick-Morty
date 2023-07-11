682a9db1f799775146f5d1e0d864cb109172d539
kuhn-moni <kuhn.moonay@gmail.com>
2023-07-11 15:38:24 +0200

    card template created

--- vite-project/src/Components/Card/Card.jsx
+++ vite-project/src/Components/Card/Card.jsx
@@ -3,11 +3,13 @@ import Button from "../Button/Button";
 import "./Card.css";
 
 function Card({ character }) {
-  const { name, gender, status, species, id } = character; //! destructuring
+  const { name, gender, status, species, id, image } = character; //! destructuring
   return (
     <div className="my-card" key={id}>
       <h1>{name}</h1>
-      <h1>{gender}</h1>
+      <h1>{species}</h1>
+      <h2>{status}</h2>
+      <h2>{image}</h2>
 
       <Button buttonLabel={"show me more"} id={id} />
     </div>
