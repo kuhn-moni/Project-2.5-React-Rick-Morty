ae058cdecd236cd51037cb12fd349045002ac369
kuhn-moni <kuhn.moonay@gmail.com>
2023-07-17 17:47:57 +0200

    add state for modal visibility and function to handle btn click

--- vite-project/src/Components/Card/Card.jsx
+++ vite-project/src/Components/Card/Card.jsx
@@ -5,6 +5,17 @@ import "./Card.css";
 
 function Card({ character }) {
   const { name, gender, status, species, id, image } = character; //! destructuring
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowMore = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="my-card" key={id}>
       <h2>{name}</h2>
@@ -12,7 +23,9 @@ function Card({ character }) {
       <h3>{status}</h3>
       <img src={image} alt={name} />
 
-      <Button buttonLabel={"show me more"} id={id} />
+      <Button buttonLabel={"show me more"} id={id} onClick={handleShowMore} />
+
+      {showModal && <Modal character={character} onClose={handleClose} />}
     </div>
   );
 }
